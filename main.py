cf312479b26cfdf4575b6ed5e72d2eec70e7597f
MOORO <fedorovmatvey48@gmail.com>
2024-11-29 18:26:32 +0300

    Задача №2 выполнена

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from PyQt5.QtGui import QPainter, QColor, QPolygon
 import sys
 import random
 
+from window import Ui_MainWindow
 
-class Example(QMainWindow):
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('01.ui', self)
+        # uic.loadUi('01.ui', self)
+        self.setupUi(self)
         self.flag = False
         self.pushButton.clicked.connect(self.draw)
         self.coords = []
@@ -18,6 +21,7 @@ class Example(QMainWindow):
     def draw(self):
         self.figure = 'circle'
         self.size = random.randint(10, 100)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.flag = True
         self.update()
 
@@ -25,9 +29,9 @@ class Example(QMainWindow):
         if self.flag:
             qp = QPainter()
             qp.begin(self)
-            qp.setPen(QColor('yellow'))
-            qp.setBrush(QColor('yellow'))
-            self.x, self.y = random.randint(100, self.sceen_size[0] - 100), random.randint(100, self.sceen_size[1] - 100)
+            qp.setPen(QColor(*self.color))
+            qp.setBrush(QColor(*self.color))
+            self.x, self.y = random.randint(100, self.sceen_size[0] - 100), random.randint(100, self.sceen_size [1] - 100)
             if self.figure == 'circle':
                 qp.drawEllipse(self.x, self.y, self.size, self.size)
             qp.end()
